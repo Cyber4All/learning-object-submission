0dff4a8f61628e6fcd7ac874047c598740c47c79
Emily Vogel <eav990@gmail.com>
2021-03-23 10:11:00 -0400

    Move routes over

--- src/modules/library-service/LibraryServiceController.ts
+++ src/modules/library-service/LibraryServiceController.ts
@@ -1,12 +1,44 @@
-import { Router } from "express";
+import { Request, Router } from "express";
+import proxy = require("express-http-proxy");
 import { Controller } from "../../interfaces/Controller";
+import * as querystring from 'querystring';
+
+const CART_API = process.env.CART_API || 'localhost:3006';
 
 export class LibraryServiceController implements Controller {
     buildRouter(): Router {
         const router = Router();
 
         // Routes go here
+        router.get(
+            '/learning-objects/metrics',
+            this.proxyRequest((req: Request) => '/learning-objects/metrics')
+        );
+        router.get(
+            '/users/:username/learning-objects/:cuid/metrics',
+            this.proxyRequest((req: Request) => `/users/${encodeURIComponent(req.params.username)}/learning-objects/${encodeURIComponent(req.params.cuid)}/metrics`)
+        );
+        router.get(
+            '/:username/library/learning-objects',
+            this.proxyRequest((req: Request) => `/users/${encodeURIComponent(req.params.username)}/library/learning-objects?${querystring.stringify(req.query)}`)
+        );
+        router.delete(
+            '/:username/library/learning-objects/:cuid',
+            this.proxyRequest((req: Request) => `/users/${encodeURIComponent(req.params.username)}/library/learning-objects/${encodeURIComponent(req.params.cuid)}?${querystring.stringify(req.query)}`)
+        );
+        router.post(
+            '/:username/library/learning-objects',
+            this.proxyRequest((req: Request) => `/users/${encodeURIComponent(req.params.username)}/library/learning-objects`)
+        );
 
         return router;
+    }
+
+    private proxyRequest(callback: Function) {
+        return proxy(CART_API, {
+            proxyReqPathResolver: req => {
+                return callback(req);
+            },
+        });
     }
 }
