e84db2c2075d60a4dc02339aef84ee4dcecb833c
Emily Vogel <eav990@gmail.com>
2021-03-26 14:28:50 -0400

    Refactor feature service

--- src/drivers/express/ExpressRouteDriver.ts
+++ src/drivers/express/ExpressRouteDriver.ts
@@ -24,7 +24,6 @@ const BUSINESS_CARD_API = process.env.BUSINESS_CARD_API || 'localhost:3009';
 const UTILITY_API = process.env.UTILITY_URI || 'localhost:9000';
 const NOTIFICATION_API = process.env.NOTIFICATION_API || 'localhost:8000';
 const OUTCOME_API = process.env.OUTCOME_API || 'localhost:3000';
-const FEATURED_API = process.env.FEATURED_API || 'localhost:3002';
 const COA_API = process.env.COA_SERVICE || 'localhost:8500';
 
 /**
@@ -57,21 +56,6 @@ export default class ExpressRouteDriver {
         message: 'Welcome to the C.L.A.R.K. Gateway API',
       });
     });
-    // FEATURED ROUTES
-    router.route('/featured/learning-objects').get(
-      proxy(FEATURED_API, {
-        proxyReqPathResolver: req => {
-          return `/featured/learning-objects`;
-        },
-      }),
-    );
-    router.route('/featured/learning-objects').patch(
-      proxy(FEATURED_API, {
-        proxyReqPathResolver: req => {
-          return `/featured/learning-objects`;
-        },
-      }),
-    );
 
      // Lambda routes
     router.route('/users/:userId/learning-objects/:learningObjectId/change-author').post(
--- src/modules/feature-service/FeatureServiceController.ts
+++ src/modules/feature-service/FeatureServiceController.ts
@@ -1,12 +1,74 @@
-import { Router } from "express";
+import { Request, Router } from "express";
+import proxy = require("express-http-proxy");
 import { Controller } from "../../interfaces/Controller";
+
+const FEATURED_API = process.env.FEATURED_API || 'localhost:3002';
 
 export class FeatureServiceController implements Controller {
     buildRouter(): Router {
         const router = Router();
 
-        // Routes go here
+        /**
+         * @swagger
+         * /featured/learning-objects:
+         *  get:
+         *      description: Gets a list of featured objects
+         *      tags:
+         *          - Feature Service
+         *      responses:
+         *          200:
+         *              description: OK
+         *              content:
+         *                  application/json:
+         *                      schema:
+         *                          type: array
+         *                          items:
+         *                              $ref: '#/components/schemas/LearningObject'
+         */
+        router.route('/featured/learning-objects').get(this.proxyRequest((req: Request) => `/featured/learning-objects`));
+
+        /**
+         * @swagger
+         * /featured/learning-objects:
+         *  patch:
+         *      description: Updates the list of featured objects
+         *      tags:
+         *          - Feature Service
+         *      requestBody:
+         *          description: The cuid, version, and author's username
+         *          required: true
+         *          content:
+         *              application/json:
+         *                  schema:
+         *                      type: array
+         *                      items:
+         *                          $ref: '#/components/schemas/LearningObject'
+         *      responses:
+         *          201:
+         *              description: CREATED
+         *              content:
+         *                  application/json:
+         *                      schema:
+         *                          type: object
+         *                          properties:
+         *                              message:
+         *                                  type: string
+         *                                  example: Learning Objects Updated
+         *          401:
+         *              description: UNAUTHENTICATED - User not logged in
+         *          403:
+         *              description: UNAUTHORIZED - User does not have privilege to update featured objects
+         */
+        router.route('/featured/learning-objects').patch(this.proxyRequest((req: Request) => `/featured/learning-objects`));
 
         return router;
+    }
+
+    private proxyRequest(callback: Function) {
+        return proxy(FEATURED_API, {
+            proxyReqPathResolver: req => {
+                return callback(req);
+            },
+        });
     }
 }
